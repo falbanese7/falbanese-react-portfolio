724c02e3f5948761b20ac249563930538357b5a3
Francesco Albanese <falbanese96@gmail.com>
2022-08-10 01:07:03 -0400

    component directory

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+      <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <Link className="text-light" to="/">
+          <h1 className="m-0">Francesco's React Portfolio</h1>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
