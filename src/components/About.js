676a7b4bace8da034e078d0bc5387bccf266abb1
Francesco Albanese <falbanese96@gmail.com>
2022-08-12 01:18:17 -0400

    About section and footer

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,33 @@
-function About() {}
+import 'bootstrap/dist/css/bootstrap.css';
+import profilePic from '../assets/images/Updated.jpg';
+
+function About() {
+  return (
+    <main>
+      <section>
+        <h1 className="welcome-header">Welcome to my portfolio!</h1>
+        <div className="hotdog d-flex justify-content-center">
+          <img id="profile-image" src={profilePic} alt="Fran with hotdog" />
+        </div>
+      </section>
+      {/* <!-- About Me --> */}
+      <section className="about container" id="about-me">
+        <div className="row">
+          <div className="col-md align-items-center section-headers">
+            <h2>About Me</h2>
+          </div>
+          <div className="col-md d-flex align-items-center left-borders">
+            <p>
+              My name is Francesco Albanese. I'm an aspiring software developer.
+              Here you'll find a little background about me, my projects, and
+              how to contact me. I'm a big fan of eating, playing video games,
+              hiking, snowboarding, and more than I can't immdiately think of.{' '}
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
 
 export default About;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,10 @@
-function Footer() {}
+function Footer() {
+  return (
+    <footer className="bg-dark">
+      {' '}
+      <pre id="heart"> Made with ❤️ by Francesco Albanese </pre>
+    </footer>
+  );
+}
 
 export default Footer;
