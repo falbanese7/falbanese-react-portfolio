91206085e464f9c9d6bfd83bda57f0369ed5c607
Francesco Albanese <falbanese96@gmail.com>
2022-08-12 03:39:53 -0400

    Resume code

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,3 +1,16 @@
-function Resume() {}
+import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import resume from '../assets/resume/Francesco Developer Resume.pdf';
+
+function Resume(props) {
+  return (
+    <MDBContainer className="mt-5">
+      <MDBRow>
+        <MDBCol md="12">
+          <embed id="Resume" src={resume} width="100%" height="750px" />
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+}
 
 export default Resume;
